4a81213d77fc9d226a62632ff25a4eb63523ab29
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-05 15:34:36 +0530

    Updated ecs-ec2

--- terraform/Modules/ecs-ec2/main.tf
+++ terraform/Modules/ecs-ec2/main.tf
@@ -33,7 +33,7 @@ resource "aws_autoscaling_group" "ecs_asg" {
   desired_capacity     = 1
   max_size             = 2
   min_size             = 1
-  vpc_zone_identifier  = var.subnet_id
+  vpc_zone_identifier  = [var.subnet_id]
   launch_template {
     id = aws_launch_template.ecs_launch_template.id
 	version = "$Latest"
