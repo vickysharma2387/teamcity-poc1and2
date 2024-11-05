83047a788b1ed647131e4601e10cefdea1dd091f
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-05 15:19:22 +0530

    Updated terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 module "vpc" {
-  source     = "./modules/vpc"
+  source     = "./Modules/vpc"
   env_name   = var.env_name
   product_name = var.product_name
 }
 
 module "ecs-ec2" {
-  source       = "./modules/ecs-ec2"
+  source       = "./Modules/ecs-ec2"
   instance_type = "t2.micro"
   security_groups = [module.security_group.security_group_id]
   env_name       = var.env_name
@@ -17,7 +17,7 @@ module "ecs-ec2" {
 }
 
 module "security_group" {
-  source       = "./modules/security_group"
+  source       = "./Modules/security_group"
   vpc_id       = module.vpc.vpc_id
   env_name     = var.env_name
   product_name = var.product_name
