85ab30f59b95a1a86acc8502d88e9c78ceb6916b
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-07 15:54:19 +0530

    Updated S3 and terraform.yml

--- terraform/Modules/s3/main.tf
+++ terraform/Modules/s3/main.tf
@@ -1,6 +1,9 @@
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "${var.product_name}-${var.env_name}-s3"
-  acl    = "private"
 }
 
+resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
+bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
 
