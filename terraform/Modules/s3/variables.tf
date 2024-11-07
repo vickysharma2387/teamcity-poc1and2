6acc2cc0dcc6eb8af5df2a3bad6e93ae47461001
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-07 15:39:34 +0530

    Updated S3

--- terraform/Modules/s3/variables.tf
+++ terraform/Modules/s3/variables.tf
@@ -1,8 +1,3 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
 variable "env_name" {
   description = "Environment name for tagging"
   type        = string
