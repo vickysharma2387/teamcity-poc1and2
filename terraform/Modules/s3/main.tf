f6a1d1d6f0d247f12a06b974906fc525ad5c0844
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-07 17:51:41 +0530

    Updated S3 main.tf

--- terraform/Modules/s3/main.tf
+++ terraform/Modules/s3/main.tf
@@ -4,7 +4,15 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "${var.product_name}-${var.env_name}-s3"
 }
  
-# S3 bucket policy with dynamic account ID
+resource "aws_s3_bucket_public_access_block" "lambda_bucket_block" {
+bucket = aws_s3_bucket.lambda_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+ 
+# S3 bucket policy with controlled access
 resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
 bucket = aws_s3_bucket.lambda_bucket.id
   policy = <<POLICY
