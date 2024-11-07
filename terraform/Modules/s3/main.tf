7a06582179dc06ea2f0d2006f6d3581ec7936750
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-07 16:59:00 +0530

    Updated S3 main.tf

--- terraform/Modules/s3/main.tf
+++ terraform/Modules/s3/main.tf
@@ -1,9 +1,28 @@
+data "aws_caller_identity" "current" {}
+ 
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "${var.product_name}-${var.env_name}-s3"
 }
-
-resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
+ 
+# S3 bucket policy with dynamic account ID
+resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
 bucket = aws_s3_bucket.lambda_bucket.id
-  acl    = "private"
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/YOUR_ROLE_NAME"
+        },
+        "Action": "s3:*",
+        "Resource": [
+          "${aws_s3_bucket.lambda_bucket.arn}",
+          "${aws_s3_bucket.lambda_bucket.arn}/*"
+        ]
+      }
+    ]
+  }
+  POLICY
 }
-
