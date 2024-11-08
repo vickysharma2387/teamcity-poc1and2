18079964c13878cefe579cdf64102bbc07d13f4d
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-08 12:30:43 +0530

    Updated variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,12 +13,3 @@ variable "product_name" {
   type        = string
 }
 
-variable "lambda_code_bucket" {
-  description = "The S3 bucket where the Lambda code zip file is stored"
-  type        = string
-}
- 
-variable "lambda_code_key" {
-  description = "The S3 key for the Lambda code zip file"
-  type        = string
-}
